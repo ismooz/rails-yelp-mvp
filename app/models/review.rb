5f14fdc174bd19dcdafa4adc90759dbb43fdb1c3
ismooz <i.gashi@pm.me>
2021-02-12 15:30:02 +0100

    Exo 2 en cours. Reste mise en forme

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: [1,2,3,4,5], allow_nil: false }
 end
